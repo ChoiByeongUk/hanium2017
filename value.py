173268e3c4745de6143810084c75d894f9b32453
ChoiByeongUk <mxg130@gmail.com>
2017-07-07 01:39:17 +0900

    value + select

--- value.py
+++ value.py
@@ -10,6 +10,7 @@ sub02_values=[]
 sub03_values=[]
 sub04_values=[]
 
+
 value.append(sub01_values)
 value.append(sub02_values)
 value.append(sub03_values)
@@ -30,7 +31,58 @@ for select in selects:
         
         
     i=i+1
+#sub 2~4에 대해 3번째 옵션을 json형식에서 취함
 
-print(value)
+json_url1='http://my.knu.ac.kr/stpo/stpo/cour/listLectPln/listCrseCdes2.action?search_open_bndle_cde='
+json_url2='&search_gubun=1'
+
+i=1
+
+select='open_crse_cde_'
+while i<=3:
+    total=[]
+    for code in value[i]:
+        temp=[]
+        temp2=[]
+        temp.append(code)
+        complete_url=json_url1+code+json_url2
+        html =urlopen(complete_url)
+        soup2 = BeautifulSoup(html,"html.parser")
+        datas=json.loads(str(soup2).replace("'",'"'))
+        if len(datas)==0:
+            continue
+        for data in datas:
+            j=1;
+            length=len(data)/2
+            while j<=length:
+                temp2.append(data[select+str(j)])
+                j=j+1
+        temp.append(temp2)
+        total.append(temp)
+
+    value[i]=total
+    i=i+1
+
+print(value[0])
+print("-----------------------------")
+print(value[1])
+print("-----------------------------")
+print(value[2])
+print("-----------------------------")
+print(value[3])
+print("-----------------------------")
+
+    
+
+
+
+
+
+
+                
+                
+            
+
+
 
 
